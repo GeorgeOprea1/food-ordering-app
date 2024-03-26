0c8141d3a7bba02004aee58151a93643ea25d1e4
GeorgeOprea <oprea.george14@yahoo.com>
2024-03-26 20:11:35 +0200

    created About Contact and Footer components

---
+++ src/components/layout/Contact.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import SectionHeaders from "@/components/layout/SectionHeaders";
+
+const Contact = () => {
+  return (
+    <section className="text-center mmy-8">
+      <SectionHeaders subHeader={"Don't hesitate"} mainHeader={"Contact Us"} />
+      <div className="mt-8">
+        <a
+          className=" text-4xl underline text-gray-500"
+          href="tel:+46738123123"
+        >
+          +46 738 123 123
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="border-t p-8 mt-16 text-center text-gray-500">
+      &copy; 2024 All rights reserved
+    </footer>
+  );
+};
+
+export default Footer;
