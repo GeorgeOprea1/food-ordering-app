de3f61ddcf6fb4ad767d8c7797a0ffd17b911895
GeorgeOprea <oprea.george14@yahoo.com>
2024-03-28 20:16:09 +0200

    Added State Management for User Registration

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,37 +1,70 @@
 "use client";
 import React, { useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const RegisterPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [userCreated, setUserCreated] = useState(false);
+  const [creatingUser, setCreatingUser] = useState(false);
+  const [error, setError] = useState(false);
 
-  function handleFormSubmit(e) {
+  async function handleFormSubmit(e) {
     e.preventDefault();
-    fetch("/api/register", {
+    setCreatingUser(true);
+    setError(false);
+    setUserCreated(false);
+    const response = await fetch("/api/register", {
       method: "POST",
       body: JSON.stringify({ email, password }),
       headers: { "Content-Type": "application/json" },
     });
+    if (response.ok) {
+      setUserCreated(true);
+    } else {
+      setError(true);
+    }
+    setCreatingUser(false);
   }
 
   return (
     <section className="mt-8">
       <h1 className="text-center text-primary text-4xl ">Register</h1>
+      {userCreated && (
+        <div className="my-4 text-center">
+          User created.
+          <br /> Now you can{" "}
+          <Link className="underline" href={"/login"}>
+            Login &raquo;
+          </Link>
+        </div>
+      )}{" "}
+      {error && (
+        <div className="my-4 text-center">
+          An error has occurred.
+          <br />
+          Please try again later
+        </div>
+      )}
       <form className="block max-w-xs mx-auto" onSubmit={handleFormSubmit}>
         <input
           type="email"
           placeholder="email"
+          disabled={creatingUser}
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
         <input
           type="password"
           placeholder="password"
+          disabled={creatingUser}
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button type="submit">Register</button>
+        <button type="submit" disabled={creatingUser}>
+          Register
+        </button>
         <div className="my-4 text-center text-gray-500">
           or log in with provider
         </div>
@@ -44,6 +77,12 @@ const RegisterPage = () => {
           ></Image>
           Login with Google
         </button>
+        <div className="text-center my-4text-gray-500 pt-4">
+          Existing account?{" "}
+          <Link className="underline" href={"/login"}>
+            Login here &raquo;
+          </Link>
+        </div>
       </form>
     </section>
   );
